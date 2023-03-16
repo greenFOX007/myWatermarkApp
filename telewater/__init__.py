1be0d46b7f6465a6e6ebad45fb2714e86390893e
DESKTOP-4BJOFMG\vova2 <vova280297@gmail.com>
2023-03-16 23:07:35 +0300

    lol11

--- telewater/__init__.py
+++
@@ -1,9 +0,0 @@
-""" A telegram bot that applies watermark on images, gifs and videos. """
-
-from importlib.metadata import version
-
-from telewater.main import start_bot
-
-__version__ = version(__package__)
-
-# AAHNIK 2021
