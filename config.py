f0603d685f1a8613aa6048b39062006759daeabd
DESKTOP-4BJOFMG\vova2 <vova280297@gmail.com>
2023-03-16 23:08:29 +0300

    lol111

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-WATERMARK = str(os.environ.get("WATERMARK", "...watermark..."))
-FONT_SIZE = int(os.environ.get("FONT_SIZE", 15))
+BOT_TOKEN = os.environ.get("5990439424:AAHFCbdr3nWyizVc6yhzVtJLCLJ7XbY90E0")
+WATERMARK = str(os.environ.get("WATERMARK", "ATN_NEWS"))
+FONT_SIZE = int(os.environ.get("FONT_SIZE", 17))
 FONT_NAME = "Space_Age.ttf"  # check tools/fonts
 TRANSPARENCY = float(os.environ.get("TRANSPARENCY", 0.75))
 QUALITY = int(os.environ.get("QUALITY", 100))
